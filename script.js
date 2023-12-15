2cf7ada03a0a1aaed9bb35644c49168140dd93e6
Muhammad Aamir Shahzad <151286354+MuhammadAamirShahzad@users.noreply.github.com>
2023-12-16 00:44:44 +0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,27 @@
+let num=prompt("Enter a Number");
+
+if(num%5==0)
+{
+    alert("The Number you entered is multiple of 5");
+
+}
+else{
+    alert("The Number you entered is not multiple of 5");
+}
+//Operators
+let a, b, c;
+b=3;
+c=4;
+console.log(b+c);
+a=2;
+;
+console.log(a++);//at this the value is still 2, but
+console.log(a);// now after prwvious operation, value became 3(increased)
+let x,y,z;
+x=5;
+x=x+1;
+console.log(x);//Answer is 6
+y=7;
+y+=1;
+console.log(y);//answer is like y=y+1 means 8
+
